fc03b13d9b609be6f1e1531fc5196ca54b5ca173
godoshian <94014358+godoshian@users.noreply.github.com>
2022-07-19 12:02:44 -0400

    refactored (Card.js && Card.css)

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,32 +1,33 @@
-// a component in react is just a function
-// ...it's a special function, of course :)
+// components in react are just functions (special functions, of course)
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-// components are also a combination of 
-// html (<div>), css (className), 
-// and javascript (function, const, {expenseTitle}, etc)
-
-////////////////////////////////////////////////////////////
-
+///////////////////////////////////////
 // Question: how do we access the values from App.js ??
 // Answer: parameters!! Well... NOT plural - only ONE param (props)
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
 export default ExpenseItem;
 
-// remember: we type "className" because this isn't technically
-// html... it's special .jsx syntax invented by react
+///////////////////////////////////////
+// Notes about this code:
+
+// 1.) components are a combination of html (<div>), css (className), 
+// and javascript (function, const, {expenseTitle}, etc)
+
+// 2.) we type "className" because this isn't technically html... 
+// it's special .jsx syntax invented by react
