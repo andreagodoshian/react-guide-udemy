d588446bf6f54fb01f33de14c969c7cd8f541093
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-07-21 15:45:41 -0400

    template literals (backticks)

--- react-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-styling-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,18 +6,32 @@ import './CourseInput.css';
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
+  const [isValid, setIsValid] = useState(true)
+
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {setIsValid(true)}
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    };
     props.onAddGoal(enteredValue);
   };
 
+  // saving for later, just in case ;)
+  // style={{borderColor: !isValid ? "red" : "black",
+  //    background: !isValid ? "pink" : "transparent"}}
+
+  // template literals (backticks)
+  // treated as a string... but can have dynamic values ${}
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
