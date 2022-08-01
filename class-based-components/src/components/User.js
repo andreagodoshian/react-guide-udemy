5eda252935743438aec148dc02ab77749c118987
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-01 11:28:07 -0400

    updated: Functional to Class-Based

--- class-based-components/src/components/User.js
+++ class-based-components/src/components/User.js
@@ -1,7 +1,18 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+/*
+1.) Classes are part of modern JavaScript; classes are not specific to React.
+2.) HOWEVER! The render method (is* specific to React
+3.) {Component} is required; it adds important properties (LIKE "PROPS")
+4.) Class-based & Functional *can* mix, but usually it's one or the other.
+*/
+class User extends Component {
+  
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+
+}
 
 export default User;
