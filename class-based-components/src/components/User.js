9d3303861b5e2230628c67bcd5ca50102530d366
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-01 14:01:32 -0400

    intro: class-based lifecycle methods

--- class-based-components/src/components/User.js
+++ class-based-components/src/components/User.js
@@ -6,8 +6,14 @@ import classes from './User.module.css';
 2.) HOWEVER! The render method (is* specific to React
 3.) {Component} is required; it adds important properties (LIKE "PROPS")
 4.) Class-based & Functional *can* mix, but usually it's one or the other.
+5.) componentWillUnmount() - if we hide (unmount) the list
 */
 class User extends Component {
+
+  // we are rendering three dummy users, so it logs for all three
+  componentWillUnmount() {
+    console.log("Users will unmount")
+  }
   
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
--- class-based-components/src/components/UserFinder.js
+++ class-based-components/src/components/UserFinder.js
@@ -1,7 +1,31 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
+
+/*
+1.) Functional has TWO state slices, so need to merge them
+2.) searchChangeHandler
+3.) <Users users={this.state.filteredUsers} />
+4.) onChange={this.searchChangeHandler.bind(this)}
+5.) We're done!! Wait... but no hooks?? (useEffect)
+^^que the life-cycle methods
+
+Doesn't this make useEffect(..., [searchTerm]) look nice?? ;)
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) => 
+        user.name.includes(this.state.searchTerm))
+      })
+    }
+  }
+
+  6.) Speaking of life-cycle methods...
+  componentDidMount() - http request, get from database
+  ^^only runs once^^
+*/
 
 const DUMMY_USERS = [
   { id: 'u1', name: 'Daria' },
@@ -9,6 +33,46 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Trent' },
 ];
 
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: ""
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) => 
+        user.name.includes(this.state.searchTerm))
+      })
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+
+}
+
+export default UserFinder;
+
+/*
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
@@ -34,3 +98,4 @@ const UserFinder = () => {
 };
 
 export default UserFinder;
+*/
