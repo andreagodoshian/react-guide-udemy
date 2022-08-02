4ca0bccde702ef64fc20a1f76d8cd0d99eb52f91
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-02 09:39:08 -0400

    updated to: async/await

--- http-requests-swapi/src/App.js
+++ http-requests-swapi/src/App.js
@@ -7,25 +7,25 @@ import './App.css';
 1.) Could use Axios... but fetch() is built-in to JavaScript and Browsers
 2.) .then(): because fetch returns a "promise"
 3.) .json(): translates, but also just a "promise"
+4.) Promises: not React-specific; it's JavaScript terminology
 */
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.py4e.com/api/films/").then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map(rawData => {
-        return {
-          id: rawData.episode_id,
-          title: rawData.title,
-          openingText: rawData.opening_crawl,
-          releaseDate: rawData.release_date
-        }
-      });
-      setMovies(transformedMovies)
-    })
+  // see below, for sytax without "async/await"
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.py4e.com/api/films/")
+    const data = await response.json()
+    const transformedMovies = data.results.map(rawData => {
+      return {
+        id: rawData.episode_id,
+        title: rawData.title,
+        openingText: rawData.opening_crawl,
+        releaseDate: rawData.release_date
+      }
+    });
+    setMovies(transformedMovies)
   }
 
   return (
@@ -41,3 +41,21 @@ function App() {
 }
 
 export default App;
+
+/*
+  function fetchMoviesHandler() {
+    fetch("https://swapi.py4e.com/api/films/").then(response => {
+      return response.json();
+    }).then(data => {
+      const transformedMovies = data.results.map(rawData => {
+        return {
+          id: rawData.episode_id,
+          title: rawData.title,
+          openingText: rawData.opening_crawl,
+          releaseDate: rawData.release_date
+        }
+      });
+      setMovies(transformedMovies)
+    })
+  }
+*/
