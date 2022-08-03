973a221afa5c4e3823d2915e7ad29fbf8fd589d7
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-08-03 11:29:54 -0400

    unit complete

--- custom-hooks-realistic/src/components/NewTask/NewTask.js
+++ custom-hooks-realistic/src/components/NewTask/NewTask.js
@@ -1,41 +1,28 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  //de-structure the object
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  // applyData(data): APPENDED to params, because of .bind()
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // "firebase name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-academind-udemy-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: 'https://react-http-academind-udemy-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: { text: taskText }, // transforms to JSON in custom-hook
+      },
+      createTask.bind(null, taskText) // .bind() preconfigures function
+    );
   };
 
   return (
